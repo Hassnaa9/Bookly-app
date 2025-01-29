5c3be7d3fce7308c3ca7ba666687b0053eb8656a
Hassnaa9 <hm2922@fayoum.edu.eg>
2025-01-29 13:29:23 +0200

    solve conflict due to merge main to master & add custom_list_view_item

--- lib/features/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -8,16 +8,13 @@ class CustomListViewItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height*.3,
-      child: AspectRatio(
-        aspectRatio: 2.8 / 4,
-        child: Container(
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              image: const DecorationImage(image: AssetImage(AssetsData.logo),
-                  fit: BoxFit.fill)
-          ),
+    return AspectRatio(
+      aspectRatio: 2.8 / 4,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: const DecorationImage(image: AssetImage(AssetsData.logo),
+                fit: BoxFit.fill)
         ),
       ),
     );
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -10,8 +10,25 @@ class ViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
-        CustomListViewItem()
+        CustomBooksListView()
       ],
+    );
+  }
+}
+class CustomBooksListView extends StatelessWidget {
+  const CustomBooksListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height*.3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+          itemBuilder: (context,index){
+          return const Padding(
+            padding:  EdgeInsets.symmetric(horizontal: 8),
+              child:  CustomListViewItem());
+          }),
     );
   }
 }
